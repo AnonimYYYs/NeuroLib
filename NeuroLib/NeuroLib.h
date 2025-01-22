ab50d7ca1a3504d169fb183fdf05fdbcb8e27dce
Danlavich <kudrenokdanil@gmail.com>
2025-01-22 20:16:14 +0300

    test commit

--- NeuroLib/NeuroLib.h
+++ NeuroLib/NeuroLib.h
@@ -7,10 +7,76 @@
 #define DLLEXPORT
 #endif
 
+#include <vector>
+#include <iostream>
+#include <math.h> 
+
+
 extern "C" {
     namespace neurolib {
         DLLEXPORT int neuro_sum(int a, int b);
     }
+}
+
+using namespace std;
+
+class Neuron;
+
+class Synapse 
+{
+private:
+    Neuron* input;
+    Neuron* output;
+    double weight;
+
+public:
+    Synapse(Neuron* in, Neuron* out, double w) : input(in), output(out), weight(w) {}
+
+    Neuron* getInput() { return input; } 
+    Neuron* getOutput() { return output; } 
+   
+    double applyWeight();
+};
+
+class Neuron 
+{
+private:
+    double value;
+    vector<Synapse*> linkedSynapses;
+
+    double activation() 
+    { 
+        // сигмоид
+        return 1.0 / (1.0 + std::exp(-value));
+    }
+
+public:
+     Neuron(double setValue = 0) : value(setValue) {}
+
+    void setValue(double setValue) { value = setValue; }
+    double getValue() { return value; }
+
+    void addSynapse(Synapse* synapse) { linkedSynapses.push_back(synapse); }
+
+    double forward(); 
+};
+
+double Synapse::applyWeight()
+{
+    double weightedInput = input->getValue() * weight;
+    output->setValue(weightedInput);
+    return weightedInput;
+}
+
+double Neuron::forward() 
+{
+    double sum = 0;
+    for (Synapse* synapse : linkedSynapses) 
+    {
+        sum += synapse->applyWeight();
+    }
+    value = sum;
+    return activation();
 }
 
 #endif //NEUROLIB_H
--- main.cpp
+++ main.cpp
@@ -3,9 +3,34 @@
 #include "NeuroLib/NeuroLib.h"
 
 
-int main() {
-    std::cout << "hello world: ";
-    std::cout << neurolib::neuro_sum(2, 4) << std::endl;
+//int main() {
+//    std::cout << "hello world: ";
+//    std::cout << neurolib::neuro_sum(2, 4) << std::endl;
+//}
+//
+//    return 0;
+using namespace std;
+
+int main() 
+{
+    vector<Neuron*> neurons;
+
+    neurons.push_back(new Neuron(0.1));
+    neurons.push_back(new Neuron(0.2));
+    neurons.push_back(new Neuron(0.3));
+    neurons.push_back(new Neuron());
+    neurons.push_back(new Neuron());
+    neurons.push_back(new Neuron());
+
+    Synapse test(neurons[0], neurons[3], 0.5);
+    neurons[0]->addSynapse(&test); 
+    neurons[3]->addSynapse(&test);
+
+    double outputValue = neurons[3]->forward(); 
+
+    cout << "Output value: " << outputValue << endl;
+    cout << "Neuron 4 value: " << neurons[3]->getValue() << endl;
+
 
     return 0;
 }
